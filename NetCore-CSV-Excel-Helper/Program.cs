056f5be5bf62adce2d74bb27c6487397c4403063
Surasith.k <Surasith.k@kabintech.co.th>
2021-06-10 12:13:52 +0700

    Add config root file path and remove excel mapper

--- NetCore-CSV-Excel-Helper/Program.cs
+++ NetCore-CSV-Excel-Helper/Program.cs
@@ -8,6 +8,7 @@ using CsvHelper;
 using CsvHelper.Configuration;
 using ExcelDataReader;
 using Ganss.Excel;
+using Microsoft.Extensions.Configuration;
 
 namespace NetCore_CSV_Excel_Helper
 {
@@ -15,21 +16,31 @@ namespace NetCore_CSV_Excel_Helper
     {
         static void Main(string[] args)
         {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{env}.json", true, true)
+                .AddEnvironmentVariables();
 
-            string excelFilePath = "telemed_patientview.xls";
-            List<Patient> patientsFromXls = ReadExcelFile<Patient>(excelFilePath, true).ToList();
+            var config = builder.Build();
 
-            string excelFilePathNoHeader = "telemed_patientview_no_header.xls";
-            List<PatientNoHeader> patientsFromXlsNoHeader = ReadExcelFile<PatientNoHeader>(excelFilePathNoHeader, false).ToList();
+            string fileRootPath = config.GetValue<string>("RootFilePath");
+            string excelFileName = "ex_with_header.xlsx";
+            string excelFileNameWitoutHeader = "ex_without_header.xlsx";
+            string csvFileName = "ex_with_header.csv";
+            string csvFileNameWithoutHeader = "ex_without_header.csv";
 
-            string csvFilePath = "telemed_patientview.csv";
-            List<Patient> patientsFromCsvWithHeader = ReadCSVFile<Patient>(csvFilePath, true).ToList();
+            string excelFilePath = Path.Combine(fileRootPath, excelFileName);
+            DataTable patientsFromXls2 = ReadExcelFile<Person>(excelFilePath, true, 1);
 
-            string csvFilePathNoHeader = "telemed_patientview_no_header.csv";
-            List<PatientNoHeader> patientsFromCsvNoHeader = ReadCSVFile<PatientNoHeader>(csvFilePathNoHeader, false).ToList();
+            string excelFilePathNoHeader = Path.Combine(fileRootPath, excelFileNameWitoutHeader);
+            DataTable patientsFromXlsNoHeader2 = ReadExcelFile<PersonNoHeader>(excelFilePathNoHeader, false, 1);
 
-            DataTable patientsFromXls2 = ReadExcelFile<Patient>(excelFilePath, true, 1);
-            DataTable patientsFromXlsNoHeader2 = ReadExcelFile<Patient>(excelFilePathNoHeader, false, 1);
+            string csvFilePath = Path.Combine(fileRootPath, csvFileName);
+            List<Person> patientsFromCsvWithHeader = ReadCSVFile<Person>(csvFilePath, true).ToList();
+
+            string csvFilePathNoHeader = Path.Combine(fileRootPath, csvFileNameWithoutHeader);
+            List<PersonNoHeader> patientsFromCsvNoHeader = ReadCSVFile<PersonNoHeader>(csvFilePathNoHeader, false).ToList();
         }
 
         static DataTable ReadExcelFile<T>(string filePath, bool hasHeader, int sheetPage)
@@ -51,114 +62,41 @@ namespace NetCore_CSV_Excel_Helper
                     });
 
                     dt = dataset.Tables[sheetPage - 1];
-
-                    //for (int i = 0; i < dt.Rows.Count; i++)
-                    //{
-                    //    Console.WriteLine($"Row {i}");
-                    //    for (int j = 0; j < dt.Columns.Count; j++)
-                    //    {
-                    //        Console.Write($"{dt.Rows[i][j]}\t");
-                    //    }
-                    //    Console.Write("\n");
-                    //}
-
-
-                    //do
-                    //{
-                    //    //T row = new T();
-                    //    while (reader.Read()) //Each ROW
-                    //    {
-                    //        var row = Activator.CreateInstance<T>();
-                    //        PropertyInfo[] properties = row.GetType().GetProperties();
-                    //        for (int column = 0; column < reader.FieldCount; column++)
-                    //        {
-                    //            //Console.WriteLine(reader.GetString(column));//Will blow up if the value is decimal etc. 
-                    //            Console.Write(reader.GetValue(column) + "\t");//Get Value returns object
-                    //            if (modelStructure.ContainsKey(column))
-                    //            {
-                    //                var _row = row.GetType().GetProperty(modelStructure[column]);
-                    //                var val = reader.GetValue(column);
-                    //                var rowType = _row.PropertyType;
-                    //                var convertedVal = Convert.ChangeType(val, rowType);
-                    //                row.GetType().GetProperty(modelStructure[column]).SetValue(row, convertedVal);
-                    //            }
-                    //        }
-                    //        Console.Write("\n");
-                    //    }
-                    //} while (reader.NextResult()); //Move to NEXT SHEET
                 }
             }
-
             return dt;
-        }
-
-        static IEnumerable<T> ReadExcelFile<T>(string filePath, bool hasHeader)
-        {
-            IEnumerable<T> rows = new List<T>();
-            if (hasHeader == false)
-            {
-                rows = new ExcelMapper(filePath) { HeaderRow = false }.Fetch<T>();
-            }
-            rows = new ExcelMapper(filePath).Fetch<T>();
-            return rows.AsEnumerable<T>();
         }
 
         static IEnumerable<T> ReadCSVFile<T>(string filePath, bool hasHeader)
         {
             IEnumerable<T> rows = new List<T>();
-            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
             {
                 HasHeaderRecord = hasHeader,
             };
-            using (var stream = new StreamReader(filePath))
+            using (var reader = new StreamReader(filePath))
+            using (var csv = new CsvReader(reader, csvConfig))
             {
-                using (var csv = new CsvReader(stream, config))
-                {
-                    csv.Read();
-                    if (hasHeader == true)
-                    {
-                        csv.ReadHeader();
-                    }
-                    rows = csv.GetRecords<T>().ToList();
-                }
+                rows = csv.GetRecords<T>().ToList();
             }
             return rows.AsEnumerable<T>();
         }
     }
 
-    class Patient
+    class Person
     {
-        public string HCODE { get; set; }
-        public string HN { get; set; }
-        public string DOB { get; set; }
-        public string MOBILENO { get; set; }
-        public string ADDRESSNOTE { get; set; }
-        public int? GENDER { get; set; }
-        public string FULLNAME { get; set; }
-        public string CID { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
     }
 
-    class PatientNoHeader
+    class PersonNoHeader
     {
-        //[Column(1)]
-        public int? No { get; set; }
+        [Column(1)]
+        public int Id { get; set; }
         [Column(2)]
-        //[Column(Letter = "B")]
-        public string HCODE { get; set; }
+        public string Name { get; set; }
         [Column(3)]
-        //[Column(Letter = "C")]
-        public string HN { get; set; }
-        [Column(4)]
-        public string DOB { get; set; }
-        [Column(5)]
-        public string MOBILENO { get; set; }
-        [Column(6)]
-        public string ADDRESSNOTE { get; set; }
-        [Column(7)]
-        public int? GENDER { get; set; }
-        [Column(8)]
-        public string FULLNAME { get; set; }
-        [Column(9)]
-        public string CID { get; set; }
+        public int Age { get; set; }
     }
 }
---
+++ Readme.md
@@ -0,0 +1,11 @@
+﻿# .NET Core CSV and Excel Helper
+
+## Features
+
+- Import and map to DTO (in Excel file with no header is not completely to map to DTO) but can map to DataSet instead
+- Export (not complete yet)
+
+## Configuration
+
+- with or without header
+- sheet page (only on Excel to DataSet)
